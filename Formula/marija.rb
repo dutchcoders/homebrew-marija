de51b098dd1349f5dccac02d6e69d1a9d791865a
Remco <remco@dutchcoders.io>
2016-11-12 13:59:41 +0100

    bumped version

--- Formula/marija.rb
+++ Formula/marija.rb
@@ -2,8 +2,8 @@ class Marija < Formula
   desc "Exploration and visualisation of Elasticsearch data"
   homepage "https://github.com/dutchcoders/marija"
   url "https://github.com/dutchcoders/marija.git",
-    :revision => "b7e5c8e215554b7eebd6fc4757768f234cabb0d8"
-  version "201611111552"
+    :revision => "ae7bb275442373a5389eacdbb01a1c6b6a215c0a"
+  version "201611121359"
 
   depends_on "go" => :build
 
